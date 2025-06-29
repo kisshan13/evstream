931157b1dfed3ad2e495d1ebbf0ca02df636a4e5
kisshan13 <kisshan07@outlook.com>
2025-06-29 09:07:11 +0530

    first commit

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    useTabs: false,
+    endOfLine: 'lf',
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    arrowParens: 'avoid',
+    parser: 'typescript',
+    quoteProps: 'as-needed',
+}
