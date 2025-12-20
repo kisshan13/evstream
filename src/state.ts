82d9cfefdfc4d6a7cd59cd49153736f06deead83
kisshan13 <kisshan07@outlook.com>
2025-12-20 23:22:28 +0530

    Added redis pub-sub for distributed reactive state which implements #4

--- src/state.ts
+++ src/state.ts
@@ -1,7 +1,7 @@
 import loadash from 'lodash'
 
 import { EvStreamManager } from './manager.js'
-import { EvStateOptions } from './types.js'
+import { EvStateAdapter, EvStateOptions } from './types.js'
 
 const { isEqual } = loadash
 
@@ -15,11 +15,36 @@ export class EvState<T> {
     #channel: string
     #manager: EvStreamManager
     #key: string
-    constructor({ channel, initialValue, manager, key }: EvStateOptions<T>) {
+    #adapter?: EvStateAdapter
+
+    constructor({ channel, initialValue, manager, key, adapter }: EvStateOptions<T>) {
         this.#value = initialValue
         this.#channel = channel
         this.#manager = manager
         this.#key = key || 'value'
+        this.#adapter = adapter
+
+        if (this.#adapter) {
+            this.#adapter.subscribe(this.#channel, (data) => {
+                this.#handleRemoteUpdate(data)
+            })
+        }
+    }
+
+    #handleRemoteUpdate(data: any) {
+        if (data && typeof data === 'object' && this.#key in data) {
+            const newValue = data[this.#key]
+
+            if (!isEqual(newValue, this.#value)) {
+                this.#value = newValue
+                this.#manager.send(this.#channel, {
+                    event: this.#channel,
+                    data: {
+                        [this.#key]: newValue,
+                    },
+                })
+            }
+        }
     }
 
     /**
@@ -44,6 +69,10 @@ export class EvState<T> {
                     [this.#key]: newValue,
                 },
             })
+
+            if (this.#adapter) {
+                this.#adapter.publish(this.#channel, { [this.#key]: newValue })
+            }
         }
     }
 }
