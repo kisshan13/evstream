be3bd22972fa9c5a9229ebafc7a78bd4dd34cace
kisshan13 <kisshan07@outlook.com>
2025-12-22 20:15:19 +0530

    Added a fix for duplicate message delivery to state , by introducing a instance id which checks before sending the data to handler. Ref: #11

--- src/adapters/redis.ts
+++ src/adapters/redis.ts
@@ -1,33 +1,41 @@
 import Redis, { RedisOptions } from 'ioredis'
 
 import { EvStateAdapter } from '../types.js'
+import { uid } from '../utils.js'
 
 export class EvRedisAdapter implements EvStateAdapter {
 	#pub: Redis
 	#sub: Redis
 	#listeners: Map<string, Set<(msg: any) => void>>
+	#instanceId: string
 
 	constructor(options?: RedisOptions) {
 		this.#pub = new Redis(options)
 		this.#sub = new Redis(options)
 		this.#listeners = new Map()
+		this.#instanceId = uid({ counter: Math.ceil(Math.random() * 100) })
 
 		this.#sub.on('message', (channel, message) => {
 			const handlers = this.#listeners.get(channel)
 			if (handlers) {
 				let parsed: any
+				let canHandle: boolean
 				try {
 					parsed = JSON.parse(message)
+					canHandle = parsed?.id !== this.#instanceId
 				} catch {
 					return
 				}
-				handlers.forEach((handler) => handler(parsed))
+				if (canHandle) handlers.forEach((handler) => handler(parsed?.message))
 			}
 		})
 	}
 
 	async publish(channel: string, message: any): Promise<void> {
-		await this.#pub.publish(channel, JSON.stringify(message))
+		await this.#pub.publish(
+			channel,
+			JSON.stringify({ id: this.#instanceId, message: message })
+		)
 	}
 
 	async subscribe(
